bc4b0a03fba87c8ee198bab9eb07fee87c6430c1
rahma <s-rahma.ebrahim@zewailcity.edu.eg>
2024-12-01 15:32:01 +0200

    Add new e-commerce project files

---
+++ website/__init__.py
@@ -0,0 +1,101 @@
+import sys
+import io
+from flask_login import LoginManager, login_user
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import secrets
+from flask_mail import Mail
+from authlib.integrations.flask_client import OAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mail = Mail()
+db = SQLAlchemy()
+oauth = OAuth()
+
+DB_NAME = 'EcommerceDB'
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
+        print('Database created successfully!')
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET")
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        'mssql+pyodbc://'
+        'LAPTOP-O3SDLSVS\\Rahma@LAPTOP-O3SDLSVS\\SQLEXPRESS/'  
+        'EcommerceDB?driver=ODBC+Driver+17+for+SQL+Server&Trusted_Connection=yes'
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+    db.init_app(app)
+
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+    app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
+    mail.init_app(app)
+
+    oauth.init_app(app)
+    oauth.register("ClickStore",
+                   client_id=os.getenv("OAUTH_CLIENT_ID"),
+                   client_secret=os.getenv("OAUTH_CLIENT_SECRET"),
+                   server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+                   client_kwargs={"scope": "openid profile email"})
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return Customer.query.get(int(id))
+
+    @app.route("/google-login")
+    def googleLogin():
+    
+        nonce = secrets.token_hex(16)
+        session['nonce'] = nonce
+        
+        redirect_uri = url_for("googleCallback", _external=True)
+        return oauth.ClickStore.authorize_redirect(redirect_uri=redirect_uri, nonce=nonce)
+
+    @app.route("/signin-google")
+    def googleCallback():
+        token = oauth.ClickStore.authorize_access_token()
+        
+        nonce = session.get('nonce')
+
+        user_info = oauth.ClickStore.parse_id_token(token, nonce=nonce)
+
+        email = user_info.get("email")
+        name = user_info.get("name")
+
+        
+        existing_customer = Customer.query.filter_by(email=email).first()
+        if not existing_customer:
+            new_customer = Customer(email=email, username=name, password_hash=None)
+            db.session.add(new_customer)
+            db.session.commit()
+            login_user(new_customer)
+        else:
+            login_user(existing_customer)
+
+        return redirect(url_for('UserController.home'))
+
+    from .user_control import UserController
+    from .auth import auth
+    from .classes import Customer, Cart, Product, Order
+
+    app.register_blueprint(UserController, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    create_database(app)
+
+    return app
---
+++ website/classes.py
@@ -0,0 +1,78 @@
+from . import db
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Customer(db.Model, UserMixin):
+    __tablename__ = 'Customers' 
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(150), nullable=False)
+    date_joined = db.Column(db.DateTime(), default=datetime.utcnow)
+    reset_token_used = db.Column(db.Boolean, default=False) 
+
+    cart_items = db.relationship('Cart', backref=db.backref('customer', lazy=True))
+    orders = db.relationship('Order', backref=db.backref('customer', lazy=True))
+      
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password=password, method='scrypt')
+    
+    def check_password(self, password):
+        print(f"Checking password: {password} against hash: {self.password_hash}")
+        return check_password_hash(self.password_hash, password)
+
+    def __str__(self):
+        return f'<Customer {self.id}>'
+
+
+
+class Product(db.Model):
+    __tablename__ = 'Products'  
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    previous_price = db.Column(db.Float, nullable=False)
+    in_stock = db.Column(db.Integer, nullable=False)
+    product_picture = db.Column(db.String(1000), nullable=False)
+    flash_sale = db.Column(db.Boolean, default=False)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    carts = db.relationship('Cart', backref=db.backref('product', lazy=True))
+    orders = db.relationship('Order', backref=db.backref('product', lazy=True))
+
+    def __str__(self):
+        return f'<Product {self.product_name}>'
+
+
+class Cart(db.Model):
+    __tablename__ = 'Cart'  
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'), nullable=False)  
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), nullable=False)  
+
+    def __str__(self):
+        return f'<Cart {self.id}>'
+
+
+class Order(db.Model):
+    __tablename__ = 'Orders'  
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    payment_id = db.Column(db.String(1000), nullable=False)
+
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'), nullable=False) 
+    product_id = db.Column(db.Integer, db.ForeignKey('Products.id'), nullable=False)  
+
+    def __str__(self):
+        return f'<Order {self.id}>'
